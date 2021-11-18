84db72a8522b454dbe899716888d095f740ee8a4
Alexandr Matveev <kareroyal@gmail.com>
2021-11-18 21:00:00 +0300

    corrected the second modul

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,6 +1,6 @@
 function sumSalary(salaries) {
   let sum = 0;
-  for (key in salaries) {
+  for (let key in salaries) {
     if (typeof(salaries[key]) == 'number' && isFinite(salaries[key])) sum += salaries[key];
     }
     return sum;
