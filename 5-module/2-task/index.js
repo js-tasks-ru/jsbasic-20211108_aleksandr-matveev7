2d563b37d577fd8d2343d99f51873c3b8a8bc14c
Alexandr Matveev <93865355+AlrMatveev@users.noreply.github.com>
2021-12-05 14:59:24 +0300

    Update index.js

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,5 +1,5 @@
 function toggleText() {
   const button = document.querySelector('.toggle-text-button');
   const text = document.getElementById('text');
-  button.addEventListener('click', () => text.getAttribute('hidden') == '' ? text.hidden = false : text.hidden = true);
+  button.addEventListener('click', () => text.hidden ? text.hidden = false : text.hidden = true);
 }
